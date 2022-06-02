19871efc594141d8626b8a7680a5257d963919ed
Claudedjuna <Abuleclaude@gmail.com>
2022-06-02 04:28:42 -0700

    task5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+ * main - program prints with function puts
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
