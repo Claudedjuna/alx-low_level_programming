68caee493798ba95e5f30d6bbcc70b77f48d0df1
Claudedjuna <Abuleclaude@gmail.com>
2022-06-09 11:37:56 -0700

    update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,15 @@
-nt _abs(int);
-int print_last_digit(int);
-int add(int, int);
-void print_to_98(int n);
-void jack_bauer(void);
-void times_table(void);
-void print_times_table(int n);
-void print_times_table(int n);
-#endf
-
+#ifndef MAIN_H
+#define MAIN_H
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void print_triangle(int size);
+void print_number(int n);
+#endif
