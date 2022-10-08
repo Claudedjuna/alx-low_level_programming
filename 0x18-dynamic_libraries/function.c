b06f6bc12fd093f1ff12e8c3eb0067fbdaf42d9b
Claudedjuna <Abuleclaude@gmail.com>
2022-10-08 06:40:18 -0700

    allow exec

---
+++ 0x18-dynamic_libraries/function.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * add - addition operator
+ * @a: first int
+ * @b: second int
+ * Return: sum of 2 numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtraction operator
+ * @a: first in
+ * @b: second int
+ * Return: difference of 2 numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - product of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: product of the numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: result of the division of the numbers
+
